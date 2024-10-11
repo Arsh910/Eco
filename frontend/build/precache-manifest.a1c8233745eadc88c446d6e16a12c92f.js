917bef43d4c8eeef610786cc5d1b5e1dfa4a0a68
Arshdeep Palial <110485426+Arsh910@users.noreply.github.com>
2024-10-11 11:14:38 +0530

    Add files via upload

---
+++ frontend/build/precache-manifest.a1c8233745eadc88c446d6e16a12c92f.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "665873604df355db2d382c8cb2448491",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7f04d1858e019c1f21aa",
+    "url": "/static/css/2.b24350a8.chunk.css"
+  },
+  {
+    "revision": "d96e537191df2b890da9",
+    "url": "/static/css/main.ee0277b8.chunk.css"
+  },
+  {
+    "revision": "7f04d1858e019c1f21aa",
+    "url": "/static/js/2.edb395df.chunk.js"
+  },
+  {
+    "revision": "86c29035efaeae79e6351d883872de72",
+    "url": "/static/js/2.edb395df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "764463235e72f86351b4",
+    "url": "/static/js/3.b7b8727a.chunk.js"
+  },
+  {
+    "revision": "d96e537191df2b890da9",
+    "url": "/static/js/main.da71d29e.chunk.js"
+  },
+  {
+    "revision": "dace90d338e32102a079",
+    "url": "/static/js/runtime-main.c5354843.js"
+  },
+  {
+    "revision": "1b580d980532792578c54897ca387e2c",
+    "url": "/static/media/Helvetica.1b580d98.ttf"
+  },
+  {
+    "revision": "57c043e0a6f3f631f1fb703f013f7142",
+    "url": "/static/media/Helvetica.57c043e0.woff2"
+  },
+  {
+    "revision": "83734009a3f9fb5ebd610de76b6dda68",
+    "url": "/static/media/Helvetica.83734009.woff"
+  },
+  {
+    "revision": "de1abb5ba15f59ad305c766bc066014b",
+    "url": "/static/media/bootVideo.de1abb5b.mp4"
+  }
+]);
