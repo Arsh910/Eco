576d9166c0b70198d9eceb28eaa88afad0608b4b
Arshdeep Palial <110485426+Arsh910@users.noreply.github.com>
2024-10-14 16:20:26 +0530

    Update middleware.py

--- backend/api/middleware.py
+++ backend/api/middleware.py
@@ -1,13 +1,15 @@
 from urllib.parse import parse_qs
 from channels.middleware import BaseMiddleware 
 from channels.db import database_sync_to_async
-import jwt
 from django.contrib.auth.models import AnonymousUser
+from rest_framework_simplejwt.tokens import AccessToken
+from django.contrib.auth import get_user_model
+import jwt
+
+User = get_user_model()
 
 @database_sync_to_async
 def get_user_from_token(token):
-    from django.contrib.auth import get_user_model  # Moved import here
-    User = get_user_model()
     try:
         # Decode the token to get the user id
         payload = jwt.decode(token, options={"verify_signature": False})
