95d4856ca1e33210c4e54f025b31e35350e142d6
Arshdeep Palial <110485426+Arsh910@users.noreply.github.com>
2024-10-15 14:34:39 +0530

    Update asgi.py

--- backend/Project/asgi.py
+++ backend/Project/asgi.py
@@ -1,20 +1,26 @@
 import os
+import django
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
-from api.middleware import JWTAuthMiddleware 
-from api import routing as api_routing 
-# from channels.auth import AuthMiddlewareStack
+from api.middleware import JWTAuthMiddleware
+from api import routing as api_routing
 
+# Ensure that the settings module is properly set
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Project.settings')
 
+# Setup Django before importing any models or middleware
+django.setup()
+
+# Get the default Django ASGI application
 django_asgi_app = get_asgi_application()
 
+# Define the ASGI application for both HTTP and WebSocket protocols
 application = ProtocolTypeRouter({
-    "http": django_asgi_app,
+    "http": django_asgi_app,  # Handles HTTP requests
     "websocket": AllowedHostsOriginValidator(
         JWTAuthMiddleware(
-            URLRouter(api_routing.websocket_urlpatterns)
+            URLRouter(api_routing.websocket_urlpatterns)  # Define WebSocket routing
         )
     ),
 })
