2ad6c6b65dca93db1d8d740043311ab98153c546
Arshdeep Palial <110485426+Arsh910@users.noreply.github.com>
2024-10-15 22:50:33 +0530

    Update settings.py

--- backend/Project/settings.py
+++ backend/Project/settings.py
@@ -167,7 +167,7 @@ CHANNEL_LAYERS = {
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES={
-        "default":dj_database_url.parse(os.getenv('HOST))
+        "default":dj_database_url.parse(os.getenv('HOST'))
 }
 
 
